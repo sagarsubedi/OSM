5aaf0edfcd7264438c093ac55ecb06cdd5cb30cd
sagarsubedi <sagarsubediofficial@gmail.com>
2021-08-20 15:34:41 -0400

    pushed to remote

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+      <div className="test">
+          <h1>This is the landing page!!</h1>
+      </div>
+  )
+}
