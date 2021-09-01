45b270ff9eed20bb19725fe85b3ca120e779a9f3
sagarsubedi <sagarsubediofficial@gmail.com>
2021-08-31 20:58:02 -0400

    created new branch

--- client/public/components/Navbar/Header.js
+++ client/public/components/Navbar/Header.js
@@ -1,28 +1,11 @@
 import classes from "./Header.module.scss";
 import Link from 'next/link';
-import {BiMenuAltRight} from 'react-icons/bi';
-import {AiOutlineClose} from 'react-icons/ai';
+
 
 export default function Header() {
     return(
-        <header className={classes.header}>
-            <div className={classes.header__content}>
-                <h2 className={classes.header__content__logo}>Navbar</h2>
-
-                <nav className={classes.header__content__nav}>
-                    <ul>
-                        <li><Link href="/"><a>Home</a></Link></li>
-                        <li><Link href="/"><a>Home</a></Link></li>
-                        <li><Link href="/"><a>Home</a></Link></li>
-                    </ul>
-                    <button>Login</button>
-                </nav>
-
-                <div className={classes.header__content__toggle}>
-                    <BiMenuAltRight />
-                </div>
-            </div>
-
-        </header>
+        <div className="temp">
+            
+        </div>
     )
 }
