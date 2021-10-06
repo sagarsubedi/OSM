17ae874588cd39e5ff9dc1c167e540cf4f7e336b
Sagar Subedi <sagarsubediofficial@gmail.com>
2021-10-06 00:03:02 -0400

    changed landing page text

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,7 +7,7 @@ import Dashboard from '../components/Dashboard/Dashboard';
 function index() {
   return (
     <div className="">
-      <h2>This is supposed to be the landing page. Go to the address bar and type: '/dashboard' after the current url without the commas to start</h2>
+      <h2>This is supposed to be the landing page. Go to the address bar and type: /dashboard after the current url without the commas to start</h2>
       <h1>Like: osm-sagarsubedi.vercel.app/dashboard</h1>
     </div>
   )
