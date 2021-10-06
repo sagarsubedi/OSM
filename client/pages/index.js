9148a22fff0df26512b9f2c609de8241f7bab4b4
Sagar Subedi <sagarsubediofficial@gmail.com>
2021-10-05 23:46:41 -0400

    reset the landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,6 +8,9 @@ export default function Home() {
 
 
   return (
-    <h2>HomePage</h2>
+    <div className="">
+      <h2>This is supposed to be the landing page. Go to the address bar and type: '/dashboard' after the current url without the commas to start</h2>
+      <h1>Like: osm-sagarsubedi.vercel.app/dashboard</h1>
+    </div>
   )
 }
