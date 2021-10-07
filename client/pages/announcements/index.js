db36cc43a84fc0abeeaaa013c08a33fe980ce6f2
Sagar Subedi <sagarsubediofficial@gmail.com>
2021-10-06 23:07:55 -0400

    added create buttons across components

--- client/pages/announcements/index.js
+++ client/pages/announcements/index.js
@@ -5,17 +5,14 @@ import Announcements from "../../components/Announcements/Announcements";
 const index = () => {
     return (
         <Layout>
-            <div className="grid flex-row gap-4 justify-center pb-6">
+            <div className="grid flex-row gap-4 justify-center pb-6 ">
+                <button className="bg-transparent hover:bg-blue-500 text-green-400 font-semibold
+                 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded tracking-widest">Create</button>
                 <h2 className="text-xl font-semibold tracking-wider text-green-200">Announcements</h2>
             </div>
 
             <Announcements />
 
-            {/* create button */}
-            <div className="flex justify-center">
-                <button className="bg-transparent hover:bg-blue-500 text-green-400 font-semibold
-                 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded tracking-widest">Create</button>
-            </div>
 
         </Layout>
     )
--- client/pages/assignments/index.js
+++ client/pages/assignments/index.js
@@ -7,6 +7,8 @@ const AssignmentsPage = () => {
     return (
         <Layout>
             <div className="grid flex-row gap-4 justify-center pb-6">
+                <button className="bg-transparent hover:bg-blue-500 text-green-400 font-semibold
+                 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded tracking-widest">Create</button>
                 <h2 className="text-xl font-semibold tracking-wider text-green-200">Assignments</h2>
             </div>
             <Assignments />
