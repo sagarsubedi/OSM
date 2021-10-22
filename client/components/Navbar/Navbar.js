bba3f66a440b2dc64a90b9f73e39b57bfc0abc2d
Sagar Subedi <sagarsubediofficial@gmail.com>
2021-10-21 20:38:27 -0400

    removed comment

--- client/components/Navbar/Navbar.js
+++ client/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
         <nav className="relative">
             <div className="flex justify-between px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
                 {Object.entries(courses).map(([key, {title, url}]) => (
-                    <Link href={url}><h2 key={key} className="cursor-pointer transition duration-100 transform hover:scale-125 active:text-red-500">{title}</h2></Link>
+                    <Link href={url} key={key}><h2  className="cursor-pointer transition duration-100 transform hover:scale-125 active:text-red-500">{title}</h2></Link>
                 ))}
             </div>
             {/* adad bg-gradient-to-l from-[#9e15ee] for last item fade-in effect */}
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.scss';
-// for global state
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
