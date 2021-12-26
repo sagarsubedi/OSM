924b664d52a2d64ff545954d915be66c1e241b49
Sagar Subedi <sagarsubediofficial@gmail.com>
2021-12-26 14:46:57 -0500

    chang <img> to Image

--- client/components/Roster/Roster.js
+++ client/components/Roster/Roster.js
@@ -1,5 +1,7 @@
 import studentDeatils from './rosterData';
 import {AcademicCapIcon, PresentationChartBarIcon} from "@heroicons/react/outline";
+import Image from "next/image";
+import Avatar from "../../public/img/avatar.png";
 
 function Roster() {
 
@@ -31,7 +33,7 @@ function Roster() {
                                 <tr key={key} className="text-black bg-blue-100 text-center">
                                     <td className="whitespace-nowrap p-5">
                                         <div className="flex items-center">
-                                            <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img className="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg" width="40" height="40" alt="user_picture" /></div>
+                                            <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><Image className="rounded-full" src={Avatar} width="40" height="40" alt="user_picture" /></div>
                                             <div className="font-medium text-gray-800">{name}</div>
                                         </div>
 
